a0ecd9b0c84d614cfd34e35992cae03394d0bd39
Andomiele <Andomiele>
2023-10-05 12:54:36 +0300

    change even.js

--- src/games/even.js
+++ src/games/even.js
@@ -2,7 +2,9 @@ import readlineSync from 'readline-sync';
 import getrandom from '../getrandom.js';
 
 const even = () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?  ');
+  console.log(`${'Hello,'} ${userName}${'!'}`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   for (let i = 0; i < 3; i += 1) {
