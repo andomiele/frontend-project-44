e47ddabbca60e5e6f8a6e805bb0967a4984f6a6f
Andomiele <Andomiele>
2023-10-09 19:01:52 +0300

    add changes

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 import getrandom from '../getrandom.js';
 
 const calc = () => {
+
   console.log('Welcome to the Brain Games!');
   
   const userName = readlineSync.question('May I have your name?  ');
