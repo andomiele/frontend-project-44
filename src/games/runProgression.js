a4cdcca89be5869c31aefe83ec9b473672dcc045
Andomiele <alina2005@ya.ru>
2023-10-23 22:09:01 +0300

    add changes

--- src/games/runProgression.js
+++ src/games/runProgression.js
@@ -6,21 +6,19 @@ const description = 'What number is missing in the progression?';
 const calculation = (first, step, length) => {
   const progression = [];
   progression[0] = first;
-  for (let i = 1; i <= length; i += 1) {
+  for (let i = 1; i < length; i += 1) {
     progression[i] = progression[i - 1] + step;
   }
   return progression;
 };
 
 const runProgression = () => {
-  const first = getRandomNumber(0, 10);
-  const step = getRandomNumber(2, 10);
+  const first = getRandomNumber(0, 5);
+  const step = getRandomNumber(1, 6);
   const length = getRandomNumber(7, 10);
-
   const correctProgression = calculation(first, step, length);
-  const randomIndex = getRandomNumber(1, correctProgression.length);
-  const answerNum = correctProgression[randomIndex];
-  const answer = answerNum.toString();
+  const randomIndex = getRandomNumber(1, correctProgression.length - 1);
+  const answer = correctProgression[randomIndex].toString();
   correctProgression[randomIndex] = '..';
   const question = `${correctProgression.join(' ')}`;
   return [question, answer];
